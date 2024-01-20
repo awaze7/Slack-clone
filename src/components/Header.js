efc4b41af35b90330760d7cd757f7383d2d45ebc
awaze7 <awazeshaikh7@gmail.com>
2024-01-20 21:16:20 +0530

    initial setup

---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import '../styles/Header.css'
+
+function Header() {
+  return (
+    <div className='header'>
+        <div className="header_user">
+            {/* Avatars for logged in user */}
+            {/* Time icon */}
+        </div>
+
+        <div className="header_search">
+            {/* Search bar */}
+            {/* Search icon */}
+        </div>
+
+        <div className="header_help">
+            {/* Help icon */}
+        </div>
+
+    </div>
+  )
+}
+
+export default Header
