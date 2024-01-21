035bd948c3c475a58002cd8f8f7d8905863c322e
awaze7 <awazeshaikh7@gmail.com>
2024-01-22 04:14:09 +0530

    added the ability to send messages in channels

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,22 +3,25 @@ import '../styles/Login.css';
 import { Button } from '@material-ui/core';
 import { auth, provider } from '../firebase';
 import { useStateValue } from './StateProvider';
+import { actionTypes } from './reducer';
 
 function Login() {
 
     const [state, dispatch] = useStateValue();
 
-    const signIn = async () => {
-        try {
-          const result = await auth.signInWithPopup(provider);
-          console.log(result.user); 
-          dispatch({
-            type: "SET_USER",
-            user: result.user,
-          })
-        } catch (error) {
-          console.error(error.message);
-        }
+    const signIn = () => {
+        auth
+        .signInWithPopup(provider)
+        .then((result) => {
+            console.log(result);
+            dispatch({
+                type: actionTypes.SET_USER,
+                user:result.user
+            });
+        })
+        .catch((error) =>{
+            alert(error.message)
+        });
     };
       
     return (
@@ -33,4 +36,4 @@ function Login() {
     )
 }
 
-export default Login
+export default Login;
