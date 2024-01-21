a0c060dad8659480df412dc46c70c8ae67461b7c
awaze7 <awazeshaikh7@gmail.com>
2024-01-21 18:51:35 +0530

    started working on the chat component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import './App.css';
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Chat from './components/Chat';
+
+function Home() {
+  return <h1>Home Screen</h1>;
+}
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Slack Clone!</h1>
+      <Router>
+        <Header />
 
-      <Header />
+        <div className="app_body">
+          <Sidebar />
 
-      <div className="app_body">
-        {/* Sidebar */}
-        <Sidebar />
-        {/* React-Router -> Chat Screen */}
-      </div>
-      
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/room/:roomId" element={<Chat />} />
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
